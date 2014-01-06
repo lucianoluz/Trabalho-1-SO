525255d0a5d2471a0411c1193b3718781e68543f
Luciano <lucianogluz@gmail.com>
2014-01-05 22:52:58 -0200

    Entrada adicionado

--- Entrada.cpp
+++ Entrada.cpp
@@ -4,12 +4,27 @@
  * 
  * Created on January 5, 2014, 9:05 PM
  */
-
+#include <string>
 #include "Entrada.h"
+#include <cstdlib>
+#include <iostream>
 
 Entrada::Entrada() {
+    this->chave = 0;
+    this->valor = "";
 }
-
+int Entrada::getChave(){
+    return this->chave;
+}
+string Entrada::getValor(){
+    return valor;
+}
+Entrada::setChave(int chave){
+    this->chave = chave;
+}
+Entrada::setValor(string valor){
+    this->valor = valor;
+}
 Entrada::Entrada(const Entrada& orig) {
 }
 
--- Entrada.h
+++ Entrada.h
@@ -4,17 +4,24 @@
  *
  * Created on January 5, 2014, 9:05 PM
  */
-
+#include <cstdlib>
+#include <iostream>
+#include <string>
 #ifndef ENTRADA_H
 #define	ENTRADA_H
-
+using namespace std;
 class Entrada {
 public:
     Entrada();
     Entrada(const Entrada& orig);
     virtual ~Entrada();
+    int getChave();
+    string getValor();
+    setChave();
+    setValor();
 private:
-
+    int chave;
+    string valor;
 };
 
 #endif	/* ENTRADA_H */
